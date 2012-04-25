57d331bb61224904ac1de4040be250837ae83553
Ed Blackburn <ed@ejb.name>
2012-04-25 22:53:43 +0100

    started first ideas for a spec and added machine.fakes ref (missed by nuget?) Removed unrequired referenes

---
+++ scr/blazey.windsor.specs/when_windsor_has_array_sub_resolver_registered.cs
@@ -0,0 +1,10 @@
+﻿namespace blazey.windsor.specs
+{
+    class when_windsor_has_array_sub_resolver_registered
+    {
+    }
+
+    class when_windsor_has_stub_sub_resolver_registered
+    {
+    }
+}

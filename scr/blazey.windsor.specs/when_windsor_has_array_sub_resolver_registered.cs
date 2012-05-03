3786e0da16a20db448e1b0e3787521f5a9b046e4
ed.blackburn <ed.blackburn@silkrouteglobal.com>
2012-05-03 17:30:36 +0100

    adding incomplete when_windsor_has_spy_sub_resolvers_they_must_be_called

--- scr/blazey.windsor.specs/when_windsor_has_array_sub_resolver_registered.cs
+++ scr/blazey.windsor.specs/when_windsor_has_array_sub_resolver_registered.cs
@@ -1,11 +1,64 @@
 ﻿using System;
+using System.Linq.Expressions;
+using Castle.Core;
+using Castle.MicroKernel;
+using Castle.MicroKernel.Context;
 using Castle.MicroKernel.Registration;
 using Castle.MicroKernel.Resolvers.SpecializedResolvers;
 using Castle.Windsor;
+using Machine.Fakes;
 using Machine.Specifications;
 
 namespace blazey.windsor.specs
 {
+    public class when_windsor_has_spy_sub_resolvers_they_must_be_called : WithFakes
+    {
+        private Establish context = () =>
+            {
+                _spySubResolver1 = An<ISubDependencyResolver>();
+                _spySubResolver2 = An<ISubDependencyResolver>();
+
+                _spySubResolver1.WhenToldTo(x => x.CanResolve(Param<CreationContext>.IsAnything,
+                                                              Param<ISubDependencyResolver>.IsAnything,
+                                                              Param<ComponentModel>.IsAnything,
+                                                              Param<DependencyModel>.IsAnything)).Return(true);
+
+                _spySubResolver2.WhenToldTo(x => x.CanResolve(Param<CreationContext>.IsAnything,
+                                                              Param<ISubDependencyResolver>.IsAnything,
+                                                              Param<ComponentModel>.IsAnything,
+                                                              Param<DependencyModel>.IsAnything)).Return(false);
+
+                _container = new WindsorContainer();
+                _container.Kernel.Resolver.AddSubResolver(_spySubResolver1);
+                _container.Kernel.Resolver.AddSubResolver(_spySubResolver2);
+                _container.Register(Component.For<object>());
+            };
+
+        private Because of = () => _exception = Catch.Exception(() => _container.Resolve<object>());
+
+        private It should_be_able_to_verify_that = () => _spySubResolver1.WasToldTo(Func);
+
+        private static Expression<Action<ISubDependencyResolver>> Func
+        {
+            get
+            {
+                return x => x.CanResolve(
+                    Param<CreationContext>.IsAnything,
+                    Param<ISubDependencyResolver>.IsAnything,
+                    Param<ComponentModel>.IsAnything,
+                    Param<DependencyModel>.IsAnything);
+            }
+        }
+
+
+        private It should_not_throw = () => _exception.ShouldBeNull();
+
+        private static IWindsorContainer _container;
+        private static ISubDependencyResolver _spySubResolver1;
+        private static ISubDependencyResolver _spySubResolver2;
+        private static Exception _exception;
+    }
+
     public class when_windsor_has_array_sub_resolver_registered
     {
         private Establish context = () =>
@@ -50,5 +103,4 @@ namespace blazey.windsor.specs
         {
         }
     }
-
 }

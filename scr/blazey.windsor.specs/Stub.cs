4e8a77e1da744f9a54631529e9fabd9da8429688
ed.blackburn <ed.blackburn@silkrouteglobal.com>
2012-06-08 12:08:54 +0100

    added more tests including a failing one that highlights I need to recognise the specification methods order of presidence

--- scr/blazey.windsor.specs/Stub.cs
+++ scr/blazey.windsor.specs/Stub.cs
@@ -1,7 +1,44 @@
+using System;
+
 namespace blazey.windsor.specs
 {
     public class Stub
     {
+        public interface IIsSatisfiedByAndMatch : IIsSatisfiedBy, IMatch
+        {
+            void SomeBehaviour(object[] foos, object[] bars);
+        }
+        
+        public interface ICanSatisfy
+        {
+            bool CanSatisfy(string param);
+        }
+
+        public interface IIsSatisfiedBy
+        {
+            bool IsSatisfiedBy(string param);
+        }
+
+        public interface ISatisfied
+        {
+            bool Satisfied(string param);
+        }
+
+        public interface ISatisfy
+        {
+            bool Satisfy(string param);
+        }
+
+        public interface IIsMatch
+        {
+            bool IsMatch(string param);
+        }
+
+        public interface IMatch
+        {
+            bool Match(string param);
+        }
+
         public interface IAmAnInterface
         {
         }
@@ -31,6 +68,27 @@ namespace blazey.windsor.specs
             public ComponentWithArrayDependency(IAmAnInterface[] things)
             {
                 Dependency = things;
+            }
+        }
+
+        public abstract class StubBase 
+        {
+            public bool IsSatisfiedBy(string param)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public class IsSatisfiedByWithIsMatchAndMatch : StubBase, IIsMatch, IMatch 
+        {
+            public bool IsMatch(string param)
+            {
+                throw new NotImplementedException();
+            }
+
+            public bool Match(string param)
+            {
+                throw new NotImplementedException();
             }
         }
     }
--- scr/blazey.windsor.specs/when_registrar_candidates_are_querried.cs
+++ scr/blazey.windsor.specs/when_registrar_candidates_are_querried.cs
@@ -5,88 +5,272 @@ using Machine.Specifications;
 
 namespace blazey.windsor.specs
 {
-    public class when_registrar_type_is_valid_candidate_with_bool_is_satisfied_member
+    public class when_registrar_item_has_can_satisfy_member
     {
         private Establish context = () =>
             {
                 _candidate = new Candidate();
-                _registrarType = typeof (Aclass);
+                _registrarType = typeof (Stub.ICanSatisfy);
                 _specificationParameterType = typeof (string);
             };
 
         private Because of = () => _exception = Catch.Exception(
             () => _specificationMethod = _candidate.SpecificationMethod(_registrarType, _specificationParameterType));
 
-        private It should_identify_as_candidate =
+        private It should_method_should_not_be_null =
             () => _specificationMethod.ShouldNotBeNull();
 
-        private It should_not_throw = () => _exception.ShouldBeNull();
+        private It should_be_can_satisfy_member_name =
+            () => _specificationMethod.Name.ShouldEqual("CanSatisfy");
+
+        private It should_have_a_first_parameter_named_param =
+            () => _specificationMethod.GetParameters()[0].Name.ShouldEqual("param");
+
+        private It should_not_throw =
+            () => _exception.ShouldBeNull();
 
         private static MethodInfo _specificationMethod;
         private static Candidate _candidate;
         private static Type _registrarType;
         private static Type _specificationParameterType;
         private static Exception _exception;
-
-        private class Aclass
-        {
-            public void Method()
-            {
-            }
-
-            public int Property { get; private set; }
-
-            public bool Parameters(int param1, string param2)
-            {
-                return false;
-            }
-
-            public bool Parameter(string param)
-            {
-                return false;
-            }
-
-            public bool CanSatisfy(string param)
-            {
-                return false;
-            }
-        }
     }
+
+    public class when_registrar_item_has_is_satisfied_by_member
+    {
+        private Establish context = () =>
+            {
+                _candidate = new Candidate();
+                _registrarType = typeof (Stub.IIsSatisfiedBy);
+                _specificationParameterType = typeof (string);
+            };
+
+        private Because of = () => _exception = Catch.Exception(
+            () => _specificationMethod = _candidate.SpecificationMethod(_registrarType, _specificationParameterType));
+
+        private It should_method_should_not_be_null =
+            () => _specificationMethod.ShouldNotBeNull();
+
+        private It should_be_is_satisfied_by_member_name =
+            () => _specificationMethod.Name.ShouldEqual("IsSatisfiedBy");
+
+        private It should_have_a_first_parameter_named_param =
+            () => _specificationMethod.GetParameters()[0].Name.ShouldEqual("param");
+
+        private It should_not_throw =
+            () => _exception.ShouldBeNull();
+
+        private static MethodInfo _specificationMethod;
+        private static Candidate _candidate;
+        private static Type _registrarType;
+        private static Type _specificationParameterType;
+        private static Exception _exception;
+    }
+
+    public class when_registrar_item_has_satisfied_member
+    {
+        private Establish context = () =>
+            {
+                _candidate = new Candidate();
+                _registrarType = typeof (Stub.ISatisfied);
+                _specificationParameterType = typeof (string);
+            };
+
+        private Because of = () => _exception = Catch.Exception(
+            () => _specificationMethod = _candidate.SpecificationMethod(_registrarType, _specificationParameterType));
+
+        private It should_method_should_not_be_null =
+            () => _specificationMethod.ShouldNotBeNull();
+
+        private It should_be_satisfiedmember_name =
+            () => _specificationMethod.Name.ShouldEqual("Satisfied");
+
+        private It should_have_a_first_parameter_named_param =
+            () => _specificationMethod.GetParameters()[0].Name.ShouldEqual("param");
+
+        private It should_not_throw =
+            () => _exception.ShouldBeNull();
+
+        private static MethodInfo _specificationMethod;
+        private static Candidate _candidate;
+        private static Type _registrarType;
+        private static Type _specificationParameterType;
+        private static Exception _exception;
+    }
+
+    public class when_registrar_item_has_satisfy_member
+    {
+        private Establish context = () =>
+            {
+                _candidate = new Candidate();
+                _registrarType = typeof (Stub.ISatisfy);
+                _specificationParameterType = typeof (string);
+            };
+
+        private Because of = () => _exception = Catch.Exception(
+            () => _specificationMethod = _candidate.SpecificationMethod(_registrarType, _specificationParameterType));
+
+        private It should_method_should_not_be_null =
+            () => _specificationMethod.ShouldNotBeNull();
+
+        private It should_be_is_satisfy_member_name =
+            () => _specificationMethod.Name.ShouldEqual("Satisfy");
+
+        private It should_have_a_first_parameter_named_param =
+            () => _specificationMethod.GetParameters()[0].Name.ShouldEqual("param");
+
+        private It should_not_throw =
+            () => _exception.ShouldBeNull();
+
+        private static MethodInfo _specificationMethod;
+        private static Candidate _candidate;
+        private static Type _registrarType;
+        private static Type _specificationParameterType;
+        private static Exception _exception;
+    }
+
+    public class when_registrar_item_has_is_match_member
+    {
+        private Establish context = () =>
+            {
+                _candidate = new Candidate();
+                _registrarType = typeof (Stub.IIsMatch);
+                _specificationParameterType = typeof (string);
+            };
+
+        private Because of = () => _exception = Catch.Exception(
+            () => _specificationMethod = _candidate.SpecificationMethod(_registrarType, _specificationParameterType));
+
+        private It should_method_should_not_be_null =
+            () => _specificationMethod.ShouldNotBeNull();
+
+        private It should_be_is_match_member_name =
+            () => _specificationMethod.Name.ShouldEqual("IsMatch");
+
+        private It should_have_a_first_parameter_named_param =
+            () => _specificationMethod.GetParameters()[0].Name.ShouldEqual("param");
+
+        private It should_not_throw =
+            () => _exception.ShouldBeNull();
+
+        private static MethodInfo _specificationMethod;
+        private static Candidate _candidate;
+        private static Type _registrarType;
+        private static Type _specificationParameterType;
+        private static Exception _exception;
+    }
+
+    public class when_registrar_item_has_match_member
+    {
+        private Establish context = () =>
+            {
+                _candidate = new Candidate();
+                _registrarType = typeof (Stub.IMatch);
+                _specificationParameterType = typeof (string);
+            };
+
+        private Because of = () => _exception = Catch.Exception(
+            () => _specificationMethod = _candidate.SpecificationMethod(_registrarType, _specificationParameterType));
+
+        private It should_method_should_not_be_null =
+            () => _specificationMethod.ShouldNotBeNull();
+
+        private It should_be_match_member_name =
+            () => _specificationMethod.Name.ShouldEqual("Match");
+
+        private It should_have_a_first_parameter_named_param =
+            () => _specificationMethod.GetParameters()[0].Name.ShouldEqual("param");
+
+        private It should_not_throw =
+            () => _exception.ShouldBeNull();
+
+        private static MethodInfo _specificationMethod;
+        private static Candidate _candidate;
+        private static Type _registrarType;
+        private static Type _specificationParameterType;
+        private static Exception _exception;
+    }
+
+    public class when_registrar_item_has_is_satisfied_by_member_and_match_member
+    {
+        private Establish context = () =>
+            {
+                _candidate = new Candidate();
+                _registrarType = typeof (Stub.IIsSatisfiedByAndMatch);
+                _specificationParameterType = typeof (string);
+            };
+
+        private Because of = () => _exception = Catch.Exception(
+            () => _specificationMethod = _candidate.SpecificationMethod(_registrarType, _specificationParameterType));
+
+        private It should_method_should_not_be_null =
+            () => _specificationMethod.ShouldNotBeNull();
+
+        private It should_be_is_satisfied_by_member_name =
+            () => _specificationMethod.Name.ShouldEqual("IsSatisfiedBy");
+
+        private It should_have_a_first_parameter_named_param =
+            () => _specificationMethod.GetParameters()[0].Name.ShouldEqual("param");
+
+        private It should_not_throw =
+            () => _exception.ShouldBeNull();
+
+        private static MethodInfo _specificationMethod;
+        private static Candidate _candidate;
+        private static Type _registrarType;
+        private static Type _specificationParameterType;
+        private static Exception _exception;
+    }
+
+    public class when_registrar_item_has_a_base_class
+    {
+        private Establish context = () =>
+        {
+            _candidate = new Candidate();
+            _registrarType = typeof(Stub.IsSatisfiedByWithIsMatchAndMatch);
+            _specificationParameterType = typeof(string);
+        };
+
+        private Because of = () => _exception = Catch.Exception(
+            () => _specificationMethod = _candidate.SpecificationMethod(_registrarType, _specificationParameterType));
+
+        private It should_method_should_not_be_null =
+            () => _specificationMethod.ShouldNotBeNull();
+
+        private It should_be_is_satisfied_by_member_name =
+            () => _specificationMethod.Name.ShouldEqual("IsSatisfiedBy");
+
+        private It should_have_a_first_parameter_named_param =
+            () => _specificationMethod.GetParameters()[0].Name.ShouldEqual("param");
+
+        private It should_not_throw =
+            () => _exception.ShouldBeNull();
+
+        private static MethodInfo _specificationMethod;
+        private static Candidate _candidate;
+        private static Type _registrarType;
+        private static Type _specificationParameterType;
+        private static Exception _exception;
+    }
+
 
     public class Candidate
     {
         public MethodInfo SpecificationMethod(Type registrarType, Type specificationParameterType)
         {
-            var candidates = registrarType.GetMembers()
-                .Where(member => member.MemberType == MemberTypes.Method)
-                .Cast<MethodInfo>()
+            var whitelistMemberName = new[] {"issatisfiedby", "satisfied", "cansatisfy", "satisfy", "ismatch", "match"};
+
+            var publicMethods = registrarType.GetInterfaces()
+                .SelectMany(contract => contract.GetMethods())
+                .Union(registrarType.GetMethods());
+
+            return publicMethods
                 .Where(member => member.ReturnType == typeof (bool))
                 .Where(member => member.GetParameters().Length == 1)
-                .Where(member => member.GetParameters()[0].ParameterType == specificationParameterType);
-
-            var specWhiteList = new[] {"issatisfiedby", "satisfied", "cansatisfy", "satisfy", "ismatch", "match"};
-
-            return candidates.FirstOrDefault(member =>
-                                             specWhiteList.Any(
-                                                 whiteItem =>
-                                                 member.Name.IndexOf(whiteItem,
-                                                                     StringComparison.CurrentCultureIgnoreCase) >= 0));
+                .Where(member => member.GetParameters()[0].ParameterType == specificationParameterType)
+                .FirstOrDefault(member => whitelistMemberName.Any(
+                    whiteItem =>
+                    whiteItem == new string(member.Name.Where(char.IsLetterOrDigit).ToArray()).ToLowerInvariant()));
         }
-    }
-
-    public class when_registrar_type_is_valid_candidate_with_bool_satisfied_member
-    {
-        private It should_identify_as_candidate;
-    }
-
-
-    public class when_registrar_type_is_valid_candidate_with_bool_is_candidate_member
-    {
-        private It should_identify_as_candidate;
-    }
-
-    public class when_registrar_type_is_valid_candidate_with_bool_matches_member
-    {
-        private It should_identify_as_candidate;
     }
 }

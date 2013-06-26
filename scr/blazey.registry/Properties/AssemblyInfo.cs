777d62d25331db08f2220a0a68e3ccae388a5f13
Ed <ed@criteriamx.com>
2013-06-26 15:55:56 +0100

    Assembly[File]Version now 0.0.0.1 for nuget.org

--- scr/blazey.registry/Properties/AssemblyInfo.cs
+++ scr/blazey.registry/Properties/AssemblyInfo.cs
@@ -1,35 +1,13 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("blazey.windsor")]
 [assembly: AssemblyDescription("Windsor Facility to allow registry and specification pattern adoption")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Blazey Software Ltd")]
+[assembly: AssemblyCompany("ed.blackburn")]
 [assembly: AssemblyProduct("blazey.windsor")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
+[assembly: AssemblyCopyright("Copyright © Blazey Software Ltd 2012, 2013")]
+[assembly: AssemblyCulture("en-gb")]
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("4bcbded2-cb11-4075-ac8b-8ebf72d95ac7")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.1")]
-[assembly: AssemblyFileVersion("1.0.0.1")]
+[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: AssemblyFileVersion("0.0.0.1")]
